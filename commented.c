125c213a5aa56126be25844a8b6e488f598e09c5
Aliyah Adeyemi-Salami <aliyahadeyemisalami246@gmail.com>
2025-09-20 23:17:34 +0100

    2nd c

---
+++ commented.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+// sleeping is more fun
+int main(void) {
+printf("Learning c is fun\n");
+return 0;
+}
---
+++ two-lines.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(void) {
+printf("My name is Aliyah\n");
+printf("I love rice\n");
+return 0;
+}
