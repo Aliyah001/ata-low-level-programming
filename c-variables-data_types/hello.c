0dcd6ac1e52c46bac1f47b19b8e3c4f10c3ebd1d
Aliyah Adeyemi-Salami <aliyahadeyemisalami246@gmail.com>
2025-09-25 21:39:48 +0100

    6th c

---
+++ c-variables-data_types/hello.c
@@ -0,0 +1,8 @@
+// c program is used  in building operating systems and enbedded dystems.
+/**  c is still relevant today because it programs run very fast and it can run on many different
+systems with little to no change.*/
+#include <stdio.h>
+int main(){
+printf ("Hello, C programming!\n" );
+return 0;
+}
