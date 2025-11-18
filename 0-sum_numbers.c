a84adbda2f290e655e9ef7dd72779172d1732685
Aliyah001 <aliyahadeyemisalami246@gmail.com>
2025-11-18 11:08:53 +0100

    Implement variadic function to sum numbers

---
+++ 0-sum_numbers.c
@@ -0,0 +1,24 @@
+include "sum_all.h"
+#include <stdarg.h>
+
+
+int sum_all(int count, ...)
+{
+     va_list args;
+    int sum = 0;
+    int i;
+    va_start(args, count);
+    for (i = 0; i < count; i++)
+    {
+
+
+        sum += va_arg(args, int);
+    }
+
+
+    va_end(args);
+
+    return sum;
+}
+
+
